f3161a1b5e192d3c2eb0ef16206fc591fa362d27
Mathieu Lorber <mathieu.lorber@gmail.com>
2020-11-08 12:04:31 +0100

    SQL refactor representant & deliberation lien

---
+++ lib-jooq/src/main/resources/data/lien_deliberation.sql
@@ -0,0 +1,12 @@
+CREATE TABLE lien_deliberation
+(
+    id              CHAR(32) PRIMARY KEY NOT NULL,
+    deliberation_id CHAR(32)             NOT NULL,
+    organisme_id    CHAR(32)             NOT NULL,
+    instance_id     CHAR(32),
+    FOREIGN KEY (deliberation_id) REFERENCES deliberation (id),
+    FOREIGN KEY (organisme_id) REFERENCES organisme (id),
+    FOREIGN KEY (instance_id) REFERENCES instance (id)
+);
+
+CREATE INDEX ON lien_deliberation (organisme_id);
---
+++ lib-jooq/src/main/resources/data/representant.sql
@@ -0,0 +1,17 @@
+CREATE TABLE representant
+(
+    id                        CHAR(32) PRIMARY KEY NOT NULL,
+    elu_id                    CHAR(32)             NOT NULL,
+    organisme_id              CHAR(32)             NOT NULL,
+    instance_id               CHAR(32),
+    position                  INT                  NOT NULL,
+    representant_or_suppleant VARCHAR(255)         NOT NULL,
+    creation_date             TIMESTAMP            NOT NULL,
+    last_modification_date    TIMESTAMP            NOT NULL,
+    FOREIGN KEY (elu_id) REFERENCES elu (id),
+    FOREIGN KEY (organisme_id) REFERENCES organisme (id),
+    FOREIGN KEY (instance_id) REFERENCES instance (id)
+);
+
+CREATE INDEX ON representant (organisme_id);
+CREATE INDEX ON representant (instance_id);
