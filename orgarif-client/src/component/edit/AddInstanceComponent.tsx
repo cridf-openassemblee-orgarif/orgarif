56eb9ebe01fe3f6986f6fe00eb3ea6366a4de995
Mathieu Lorber <mathieu.lorber@gmail.com>
2020-11-11 15:47:47 +0100

    Reset add instance form at submit

--- orgarif-client/src/component/edit/AddInstanceComponent.tsx
+++ orgarif-client/src/component/edit/AddInstanceComponent.tsx
@@ -1,8 +1,11 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/core';
+import { useState } from 'react';
 import { appContext } from '../../ApplicationContext';
 import { OrganismeId } from '../../domain/id';
+import { stringifyNominalString } from '../../domain/nominal-class';
 import { FullInstance } from '../../domain/organisme';
+import { clientUid } from '../../utils';
 import { SimpleForm } from '../base-component/SimpleForm';
 import { TextInput } from '../base-component/TextInput';
 
@@ -11,11 +14,14 @@ export const AddInstanceComponent = (props: {
   instances: FullInstance[];
   setInstances: (instances: FullInstance[]) => void;
 }) => {
+  // key to reset the form
+  const [key, setKey] = useState(clientUid());
   const addInstance = (nomInstance: string) => {
     appContext
       .commandService()
       .addInstanceCommand({ nomInstance, organismeId: props.organismeId })
       .then((r) => {
+        setKey(clientUid);
         const instance: FullInstance = {
           infos: {
             id: r.id,
@@ -31,7 +37,10 @@ export const AddInstanceComponent = (props: {
       });
   };
   return (
-    <SimpleForm onSubmit={(e) => addInstance(e.nom)}>
+    <SimpleForm
+      key={stringifyNominalString(key)}
+      onSubmit={(e) => addInstance(e.nom)}
+    >
       <TextInput name="nom" label="Nouvelle instance" />
     </SimpleForm>
   );
