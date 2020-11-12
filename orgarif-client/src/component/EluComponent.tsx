876fdf46369bd7e2a44a8e19119a3d372ae261aa
Mathieu Lorber <mathieu.lorber@gmail.com>
2020-11-12 13:03:08 +0100

    s

--- orgarif-client/src/component/EluComponent.tsx
+++ orgarif-client/src/component/EluComponent.tsx
@@ -1,16 +1,37 @@
 /** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import * as React from 'react';
 import { useRecoilValue } from 'recoil';
 import { EluId } from '../domain/id';
 import { get } from '../domain/nominal-class';
 import { state } from '../state/state';
+import { colors } from '../styles/vars';
 
 export const EluComponent = (props: { eluId: EluId }) => {
   const elusById = useRecoilValue(state.elusById);
   const e = get(elusById, props.eluId);
   return (
-    <p>
-      {e.prenom} {e.nom}
-    </p>
+    <div>
+      <div
+        css={css`
+          height: 50px;
+          width: 50px;
+          overflow: hidden;
+          text-align: center;
+          background: ${colors.white};
+        `}
+      >
+        <img
+          src={`http://localhost:8080${e.imageUrl}`}
+          css={css`
+            max-height: 50px;
+            max-width: 50px;
+          `}
+        />
+      </div>
+      <p>
+        {e.prenom} {e.nom}
+      </p>
+    </div>
   );
 };
