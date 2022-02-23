1b762488afa051f14ba7f24b56e489be6ed2f109
Mathieu Derval <ddmunky@users.noreply.github.com>
2022-02-23 17:52:15 +0100

    :bento: - adding new icons assets

---
+++ orgarif-client/src/icon/collection/Mail.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import * as icon from '../Icon';
+
+export const Mail = styled(function Mail(
+  props: icon.SpecificIconProps
+): React.ReactElement {
+  return (
+    <icon.Icon {...props} name="mail" viewBox="-10 0 963 800">
+      <path d="M772 150v493H170V150h602zm-55 55H225v383h492V205zm-427 42 183 157 183-157 36 42-219 187-218-187z" />
+    </icon.Icon>
+  );
+})``;
--- orgarif-client/src/icon/collection/NatureJuridique.tsx
+++ orgarif-client/src/icon/collection/NatureJuridique.tsx
@@ -1,11 +1,11 @@
 import styled from '@emotion/styled';
 import * as icon from '../Icon';
 
-export const Organigram = styled(function Organigram(
+export const NatureJuridique = styled(function NatureJuridique(
   props: icon.SpecificIconProps
 ): React.ReactElement {
   return (
-    <icon.Icon {...props} name="organigram" viewBox="-10 0 874 1000">
+    <icon.Icon {...props} name="nature juridique" viewBox="-10 0 963 800">
       <path d="M709 533h85v217H578V533h84V417H460v116h80v217H324V533h89V417H205v116h81v217H70V533h88V370h255V267h-87V50h216v217h-82v103h249v163zM373 97v121h124V97H373zM241 701V580H117v121h124zm254 0V580H371v121h124zm254 0V580H625v121h124z" />
     </icon.Icon>
   );
