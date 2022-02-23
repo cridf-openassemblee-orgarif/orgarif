87c71414bd5a5e8427a5d59ed1fe1079a9116fd1
Mathieu Derval <ddmunky@users.noreply.github.com>
2022-02-23 17:52:15 +0100

    :bento: - adding new icons assets

---
+++ orgarif-client/src/icon/collection/Localite.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import * as icon from '../Icon';
+
+export const Localite = styled(function Localite(
+  props: icon.SpecificIconProps
+): React.ReactElement {
+  return (
+    <icon.Icon {...props} name="localité" viewBox="-10 0 989 800">
+      <path d="M482 775 250 461c-36-50-56-109-56-171 0-77 30-150 85-205S407 0 484 0c78 0 151 30 205 85 55 55 85 128 85 205 0 65-21 127-61 178zm2-719c-129 0-234 105-234 234 0 50 15 98 45 139l187 254 187-248v-1c32-41 50-91 50-144 0-129-106-234-235-234zm0 386c-37 0-71-15-96-42-23-25-36-59-36-94 0-37 13-70 36-96 25-27 59-42 96-42s72 15 96 42c24 25 37 59 37 96 0 35-13 69-37 95s-59 41-96 41zm0-218c-50 0-77 42-77 82 0 39 27 81 77 81 51 0 78-42 78-81 0-47-34-82-78-82z" />
+    </icon.Icon>
+  );
+})``;
---
+++ orgarif-client/src/icon/collection/Representant.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import * as icon from '../Icon';
+
+export const Representant = styled(function Representant(
+  props: icon.SpecificIconProps
+): React.ReactElement {
+  return (
+    <icon.Icon {...props} name="représentant" viewBox="-10 0 963 800">
+      <path d="M371 424c-106 0-192-86-192-192S265 39 371 39s192 87 192 193-86 192-192 192zm0-329c-76 0-137 61-137 137 0 75 61 137 137 137s137-62 137-137c0-76-61-137-137-137zm247 655h-56v-29c0-105-86-191-191-191-106 0-191 86-191 191v29h-56v-29c0-136 111-246 247-246s247 110 247 246v29z" />
+    </icon.Icon>
+  );
+})``;
