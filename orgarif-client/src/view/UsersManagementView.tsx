2b53247a9c48af1f7973681456bf09b6109aa0e9
Mathieu Lorber <mathieu.lorber@gmail.com>
2022-04-13 13:09:32 +0200

    WIP fmk 0.0.3

--- orgarif-client/src/view/UsersManagementView.tsx
+++ orgarif-client/src/view/UsersManagementView.tsx
@@ -2,10 +2,9 @@
 import * as React from 'react';
 import { MainContainer } from '../container/MainContainer';
 
-export const UsersManagementView = () => {
-  return (
-    <MainContainer>
-      <h1>Users management</h1>
-    </MainContainer>
-  );
-};
+export const UsersManagementView = () => (
+  <MainContainer>
+    <h1>Users management</h1>
+    <div>hi</div>
+  </MainContainer>
+);
