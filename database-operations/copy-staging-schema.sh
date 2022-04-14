1c9b381bf74036cce0e870ab56324c2eb525c3ec
Mathieu Lorber <mathieu.lorber@gmail.com>
2022-04-14 10:44:47 +0200

    [scripts] Copy prod & staging schemas

---
+++ database-operations/copy-staging-schema.sh
@@ -0,0 +1,7 @@
+set -e
+dropdb orgarif-staging-schema || true
+echo 'ok drop'
+pg_dump -h bjg5euhjrhastc12cumm-postgresql.services.clever-cloud.com -p 5562 -U u6ntkekippqttzxk2gii -d bjg5euhjrhastc12cumm \
+  -n public --schema-only > $ORGARIF_WORK_DIR/db/staging-schema.sql
+createdb orgarif-staging-schema
+psql -d orgarif-staging-schema < $ORGARIF_WORK_DIR/db/staging-schema.sql
