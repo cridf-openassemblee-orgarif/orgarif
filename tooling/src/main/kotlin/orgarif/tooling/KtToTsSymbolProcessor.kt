8a9c2d26e9ff94689786b4ffba24bc33431d3c91
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-03-02 11:40:59 +0100

    [tmpl] Kt to ts generation : prettier & move are done asynchronously (they made the build terribly slow)

--- tooling/src/main/kotlin/orgarif/tooling/KtToTsSymbolProcessor.kt
+++ tooling/src/main/kotlin/orgarif/tooling/KtToTsSymbolProcessor.kt
@@ -20,7 +20,6 @@ import orgarif.tooling.kttots.prettyPrint
 import java.nio.file.Files
 import java.time.LocalDateTime
 import kotlin.io.path.absolutePathString
-import kotlin.io.path.moveTo
 import kttots.Shared
 
 // TODO[tmpl] use exceptions and catch them for debug report ?
@@ -37,19 +36,25 @@ class KtToTsSymbolProcessor(
     val options: Map<String, String>
 ) : SymbolProcessor {
     override fun process(resolver: Resolver): List<KSAnnotated> {
-        val millis = System.currentTimeMillis()
-        val configuration = KtToTsConfiguration.init(options)
-        val debugReport = if (configuration.debugFile != null) StringBuilder() else null
-        debugReport?.appendLine("<html><body><pre>")
-        debugReport?.appendLine("Start generation ${LocalDateTime.now()}")
-        debugReport?.appendLine("<h1>Configuration</h1>")
-        debugReport?.appendLine(configuration.prettyPrint())
+        val startTime = System.currentTimeMillis()
         val symbols =
             resolver
                 .getSymbolsWithAnnotation(Shared::class.java.name)
                 .filterIsInstance<KSClassDeclaration>()
         // TODO[tmpl] what happens if no file ?
         if (!symbols.iterator().hasNext()) return emptyList()
+        processFiles(symbols, startTime)
+        val unableToProcess = symbols.filterNot { it.validate() }.toList()
+        return unableToProcess
+    }
+
+    fun processFiles(symbols: Sequence<KSClassDeclaration>, startTime: Long) {
+        val configuration = KtToTsConfiguration.init(options)
+        val debugReport = if (configuration.debugFile != null) StringBuilder() else null
+        debugReport?.appendLine("<html><body><pre>")
+        debugReport?.appendLine("Start generation ${LocalDateTime.now()}")
+        debugReport?.appendLine("<h1>Configuration</h1>")
+        debugReport?.appendLine(configuration.prettyPrint())
         debugReport?.apply {
             appendLine("<h1>Initial symbols selection</h1>")
             symbols.forEach { appendLine("$it") }
@@ -161,12 +166,33 @@ class KtToTsSymbolProcessor(
                     ksFile to file
                 }
         //        debugReport?.let {
-        //            typesSelection.map { debugReport.appendLine("${it.qualifiedName?.asString()}")
+        //            typesSelection.map {
+        // debugReport.appendLine("${it.qualifiedName?.asString()}")
         // }
         //        }
+        resultFiles.forEach {
+            // TODO[fmk] format before writing file to avoid triggering webpack hot reload, useless
+            // temporary diffs...
+            ShellRunner.launch(
+                configuration.clientDirectory,
+                // FIXME[fmk] makes this macos only
+                "/usr/local/bin/node",
+                "node_modules/prettier/bin-prettier.js",
+                "--config",
+                "package.json",
+                "--write",
+                it.second.absolutePathString(),
+                "&&",
+                "mv",
+                it.second.absolutePathString(),
+                configuration.srcDirectory
+                    .resolve(kotlinToTsFile(it.first, configuration))
+                    .absolutePathString())
+        }
+        //        tempDir.toFile().deleteRecursively()
         debugReport?.appendLine("<h1>Report</h1>")
         debugReport?.appendLine("Finished generation ${LocalDateTime.now()}")
-        debugReport?.appendLine("Took ${System.currentTimeMillis() - millis}ms")
+        debugReport?.appendLine("Took ${System.currentTimeMillis() - startTime}ms")
         debugReport?.appendLine("<h1>Debug autre</h1>")
         debugReport?.appendLine(Debug.sb.toString())
         debugReport?.appendLine("</pre></body></html>")
@@ -177,23 +203,5 @@ class KtToTsSymbolProcessor(
             configuration.debugFile.parentFile.mkdirs()
             configuration.debugFile.writeText(debugReport.toString())
         }
-        val unableToProcess = symbols.filterNot { it.validate() }.toList()
-        resultFiles.forEach {
-            // TODO[fmk] format before writing file to avoid triggering webpack hot reload
-            ShellRunner.run(
-                configuration.clientDirectory,
-                // FIXME[fmk] makes this macos only
-                "/usr/local/bin/node",
-                "node_modules/prettier/bin-prettier.js",
-                "--config",
-                "package.json",
-                "--write",
-                it.second.absolutePathString())
-            it.second.moveTo(
-                configuration.srcDirectory.resolve(kotlinToTsFile(it.first, configuration)),
-                overwrite = true)
-        }
-        tempDir.toFile().deleteRecursively()
-        return unableToProcess
     }
 }
