1797d6f0d77fc9943e89d4f02b5abab7b2948635
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-03-02 11:40:13 +0100

    [tmpl] Query read transaction

--- orgarif-server/src/main/kotlin/orgarif/controller/QueryController.kt
+++ orgarif-server/src/main/kotlin/orgarif/controller/QueryController.kt
@@ -9,6 +9,7 @@ import orgarif.query.QueryResponse
 import orgarif.repository.user.UserDao
 import orgarif.serialization.Serializer
 import orgarif.service.user.UserSessionService
+import orgarif.service.utils.TransactionIsolationService
 import java.net.URLDecoder
 import javax.servlet.http.HttpServletRequest
 import org.springframework.web.bind.annotation.GetMapping
@@ -18,7 +19,8 @@ import org.springframework.web.bind.annotation.RestController
 class QueryController(
     val userDao: UserDao,
     val userSessionService: UserSessionService,
-    val isMailAlreadyTakenQueryHandler: IsMailAlreadyTakenQueryHandler
+    val isMailAlreadyTakenQueryHandler: IsMailAlreadyTakenQueryHandler,
+    val transactionIsolationService: TransactionIsolationService,
 ) {
 
     @GetMapping("/query")
@@ -30,7 +32,7 @@ class QueryController(
         val handler = handler(query)
         val userSession =
             if (userSessionService.isAuthenticated()) userSessionService.getUserSession() else null
-        return handler.doHandle(query, userSession)
+        return transactionIsolationService.executeReadOnly { handler.doHandle(query, userSession) }
     }
 
     private fun handler(query: Query) =
