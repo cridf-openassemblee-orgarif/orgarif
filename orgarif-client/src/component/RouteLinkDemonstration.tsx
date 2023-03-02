b5ced9cfade6deec77f0d130c035fb701b65ce8e
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-03-02 11:40:34 +0100

    [tmpl] µrefactor subcomponents

--- orgarif-client/src/component/RouteLinkDemonstration.tsx
+++ orgarif-client/src/component/RouteLinkDemonstration.tsx
@@ -30,109 +30,121 @@ export const RouteLinkDemonstration = () => {
           }
         `}
       >
-        <div>
-          <p>NoMatch</p>
-          <p>
-            <RouteLink
-              route={{ name: 'TestRoute' }}
-              css={styles.link}
-              element="Button"
-            >
-              TestRoute
-            </RouteLink>
-          </p>
-          <p>
-            <RouteLink
-              route={{ name: 'TestSubRoute' }}
-              css={styles.link}
-              element="Button"
-            >
-              TestSubRoute
-            </RouteLink>
-          </p>
-          <p>
-            <RouteLink
-              route={{ name: 'TestSubIdRoute', id: 'id' }}
-              css={styles.link}
-              element="Button"
-            >
-              TestSubIdRoute
-            </RouteLink>
-          </p>
-        </div>
-        <div>
-          <p>FullMatch</p>
-          <p>
-            <MatchRouteLink
-              route={{ name: 'TestRoute' }}
-              matchModel="FullMatch"
-              css={styles.link}
-              activeCss={styles.activeLink}
-              element="Button"
-            >
-              TestRoute
-            </MatchRouteLink>
-          </p>
-          <p>
-            <MatchRouteLink
-              route={{ name: 'TestSubRoute' }}
-              matchModel="FullMatch"
-              css={styles.link}
-              activeCss={styles.activeLink}
-              element="Button"
-            >
-              TestSubRoute
-            </MatchRouteLink>
-          </p>
-          <p>
-            <MatchRouteLink
-              route={{ name: 'TestSubIdRoute', id: 'id' }}
-              matchModel="FullMatch"
-              css={styles.link}
-              activeCss={styles.activeLink}
-              element="Button"
-            >
-              TestSubIdRoute
-            </MatchRouteLink>
-          </p>
-        </div>
-        <div>
-          <p>PartialMatch</p>
-          <p>
-            <MatchRouteLink
-              route={{ name: 'TestRoute' }}
-              matchModel="PartialMatch"
-              css={styles.link}
-              activeCss={styles.activeLink}
-              element="Button"
-            >
-              TestRoute
-            </MatchRouteLink>
-          </p>
-          <p>
-            <MatchRouteLink
-              route={{ name: 'TestSubRoute' }}
-              matchModel="PartialMatch"
-              css={styles.link}
-              activeCss={styles.activeLink}
-              element="Button"
-            >
-              TestSubRoute
-            </MatchRouteLink>
-          </p>
-          <p>
-            <MatchRouteLink
-              route={{ name: 'TestSubIdRoute', id: 'id' }}
-              matchModel="PartialMatch"
-              css={styles.link}
-              activeCss={styles.activeLink}
-              element="Button"
-            >
-              TestSubIdRoute
-            </MatchRouteLink>
-          </p>
-        </div>
+        <NoMatch />
+        <PartialMatch />
+        <FullMatch />
       </div>
     </>
   );
 };
+
+const NoMatch = () => (
+  <div>
+    <p>NoMatch</p>
+    <p>
+      <RouteLink
+        route={{ name: 'TestRoute' }}
+        css={styles.link}
+        element="Button"
+      >
+        TestRoute
+      </RouteLink>
+    </p>
+    <p>
+      <RouteLink
+        route={{ name: 'TestSubRoute' }}
+        css={styles.link}
+        element="Button"
+      >
+        TestSubRoute
+      </RouteLink>
+    </p>
+    <p>
+      <RouteLink
+        route={{ name: 'TestSubIdRoute', id: 'id' }}
+        css={styles.link}
+        element="Button"
+      >
+        TestSubIdRoute
+      </RouteLink>
+    </p>
+  </div>
+);
+
+const PartialMatch = () => (
+  <div>
+    <p>PartialMatch</p>
+    <p>
+      <MatchRouteLink
+        route={{ name: 'TestRoute' }}
+        matchModel="PartialMatch"
+        css={styles.link}
+        activeCss={styles.activeLink}
+        element="Button"
+      >
+        TestRoute
+      </MatchRouteLink>
+    </p>
+    <p>
+      <MatchRouteLink
+        route={{ name: 'TestSubRoute' }}
+        matchModel="PartialMatch"
+        css={styles.link}
+        activeCss={styles.activeLink}
+        element="Button"
+      >
+        TestSubRoute
+      </MatchRouteLink>
+    </p>
+    <p>
+      <MatchRouteLink
+        route={{ name: 'TestSubIdRoute', id: 'id' }}
+        matchModel="PartialMatch"
+        css={styles.link}
+        activeCss={styles.activeLink}
+        element="Button"
+      >
+        TestSubIdRoute
+      </MatchRouteLink>
+    </p>
+  </div>
+);
+
+const FullMatch = () => (
+  <div>
+    <p>FullMatch</p>
+    <p>
+      <MatchRouteLink
+        route={{ name: 'TestRoute' }}
+        matchModel="FullMatch"
+        css={styles.link}
+        activeCss={styles.activeLink}
+        element="Button"
+      >
+        TestRoute
+      </MatchRouteLink>
+    </p>
+    <p>
+      <MatchRouteLink
+        route={{ name: 'TestSubRoute' }}
+        matchModel="FullMatch"
+        css={styles.link}
+        activeCss={styles.activeLink}
+        element="Button"
+      >
+        TestSubRoute
+      </MatchRouteLink>
+    </p>
+    <p>
+      <MatchRouteLink
+        route={{ name: 'TestSubIdRoute', id: 'id' }}
+        matchModel="FullMatch"
+        css={styles.link}
+        activeCss={styles.activeLink}
+        element="Button"
+      >
+        TestSubIdRoute
+      </MatchRouteLink>
+    </p>
+  </div>
+);
