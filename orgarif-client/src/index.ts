475573feeca4d5c75a2af508c1953f9e9792bcc4
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-03-02 11:40:37 +0100

    [tmpl] Fix to avoid auto sorting on index.ts

--- orgarif-client/src/index.ts
+++ orgarif-client/src/index.ts
@@ -1,8 +1,6 @@
-// organize-imports-ignore
 /**
  * [doc] this file exists to control important imports order
- * auto prettier-plugin-organize-imports reordering is blocked by 'organize-imports-ignore'
- * polyfills must be loaded first in case of things done 'synchronously' in files
+ * is in .prettierignore to avoid prettier-plugin-sort-imports
  */
 import 'es6-promise/auto';
 import 'es6-shim';
