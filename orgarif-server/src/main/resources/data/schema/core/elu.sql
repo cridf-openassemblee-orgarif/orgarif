2b33d4f792411799867d8d0663466f2ba3f79b27
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-03-13 23:19:31 +0100

    SQL image url can be null

--- orgarif-server/src/main/resources/data/schema/core/elu.sql
+++ orgarif-server/src/main/resources/data/schema/core/elu.sql
@@ -6,7 +6,7 @@ CREATE TABLE elu
     nom VARCHAR(255) NOT NULL,
     groupe_politique VARCHAR(255) NOT NULL,
     groupe_politique_court VARCHAR(255) NOT NULL,
-    image_url VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255),
     actif BOOLEAN NOT NULL,
     creation_date TIMESTAMPTZ NOT NULL,
     last_modification_date TIMESTAMPTZ NOT NULL
