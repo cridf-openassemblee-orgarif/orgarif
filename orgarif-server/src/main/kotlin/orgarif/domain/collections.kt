23b0850c26a51880d804f45fbef6c0937cd68f46
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-06-10 11:17:40 +0200

    [tmpl] Add firstAndOnlyOne { condition }

--- orgarif-server/src/main/kotlin/orgarif/domain/collections.kt
+++ orgarif-server/src/main/kotlin/orgarif/domain/collections.kt
@@ -6,3 +6,11 @@ fun <T> List<T>.firstAndOnlyOne(): T {
     }
     return first()
 }
+
+fun <T> List<T>.firstAndOnlyOne(select: (v: T) -> Boolean): T {
+    val s = filter(select)
+    if (s.size != 1) {
+        throw IllegalArgumentException("Collection has ${s.size} elements.")
+    }
+    return s.first()
+}
