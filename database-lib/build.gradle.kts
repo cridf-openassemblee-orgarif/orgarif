74fa8c04fe74ba77df3fd495297ef3fe15f5a957
Mathieu Lorber <mathieu.lorber@gmail.com>
2023-07-16 20:58:28 +0200

    [tmpl] Kotlin 1.9.0

--- database-lib/build.gradle.kts
+++ database-lib/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins { kotlin("jvm") }
 
-val kotlinVersion = "1.8.21"
+val kotlinVersion = "1.9.0"
 val jooqVersion = "3.18.4"
 
 tasks {
